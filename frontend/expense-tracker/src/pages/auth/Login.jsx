decb49fb19486588cb48b751697659fdac340059
Sufia Naz <sufia1904@gmail.com>
2025-11-19 02:11:19 +0530

    login page

--- frontend/expense-tracker/src/pages/auth/Login.jsx
+++ frontend/expense-tracker/src/pages/auth/Login.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from 'react';
+import './login.css';
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className="login-container">
+      <div className="login-box">
+        {/* Top text */}
+        <div className="top-text">
+          <h2>Welcome to FinTrack</h2>
+          <p>Please login to continue</p>
+        </div>
 
+        {/* Skewed 3D form */}
+        <form className="form">
+          <ul className="wrapper">
+            <li style={{ "--i": 4 }}>
+              <input className="input" type="text" placeholder="Name" required />
+            </li>
+            <li style={{ "--i": 3 }}>
+              <input className="input" type="text" placeholder="Phone number" required name="phone" />
+            </li>
+            <li style={{ "--i": 2 }}>
+              <input className="input" type="email" placeholder="E-mail" required name="email" />
+            </li>
+            <li style={{ "--i": 1 }}>
+              <button type="submit">Submit</button>
+            </li>
+          </ul>
+        </form>
+
+        {/* Bottom text */}
+        <div className="bottom-text">
+          <p>Don't have an account? <span className="signup-link">Sign Up</span></p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
